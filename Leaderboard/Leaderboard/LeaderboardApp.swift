f2337865a3787e286a3060d7ab77d7c813305c8d
Vicente Úbeda <appeleando@gmail.com>
2021-06-29 23:14:14 -0400

    Create Xcode project

---
+++ Leaderboard/Leaderboard/LeaderboardApp.swift
@@ -0,0 +1,16 @@
+// LeaderboardApp.swift
+// Leaderboard
+//
+// Creado el 29/6/21 por Vicente Úbeda (@vicentube)
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct LeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
